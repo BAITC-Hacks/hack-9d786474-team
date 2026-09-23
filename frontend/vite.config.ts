7b10036597b9759524ff51e6a14d67b8e09465c7
Yernar <yernaribadulla@gmail.com>
2026-09-23 17:15:04 +0500

    Add profiles and polished app shell

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/tasks': 'http://127.0.0.1:8000', '/catalog': 'http://127.0.0.1:8000', '/teams': 'http://127.0.0.1:8000', '/proposals': 'http://127.0.0.1:8000' } },
+  server: { proxy: { '/tasks': 'http://127.0.0.1:8000', '/catalog': 'http://127.0.0.1:8000', '/teams': 'http://127.0.0.1:8000', '/proposals': 'http://127.0.0.1:8000', '/profiles': 'http://127.0.0.1:8000' } },
 });
